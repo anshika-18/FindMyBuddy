784dc5ae1ab2994545c74e2607d48b96df00265f
Anshika Jain <anshikajain@Anshikas-MacBook-Air.local>
2023-05-05 15:38:32 +0530

    Bcrypt Js

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,21 +1,42 @@
 import clientPromise from "../../lib/mongo/index";
 
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const salt = 10;
+
 export default async (req, res) => {
   try {
     if (!req.body.email || !req.body.password)
       return res.json({ error: "Please enter all fields" });
     const email = req.body.email;
-    const password = req.body.password;
+    let password = req.body.password;
     const client = await clientPromise;
     const db = client.db("users");
-
-    const users = await db
-      .collection("users")
-      .find({ email: email, password: password })
-      .toArray();
-
+    const users = await db.collection("users").find({ email: email }).toArray();
     if (users.length == 0) return res.json({ error: "User not found" });
-    else return res.status(200).json(users[0]);
+    else {
+      console.log(users[0]);
+      const isMatch = await bcrypt.compare(password, users[0].password);
+      //console.log(isMatch);
+      if (!isMatch) {
+        return res.json({ error: "Invalid Credentials" });
+      } else {
+        await jwt.sign(
+          { id: users[0]._id },
+          process.env.jwtSecret,
+
+          (err, token) => {
+            if (err) throw err;
+            return res.json({
+              token,
+              email: users[0].email,
+              password: users[0].password,
+            });
+          }
+        );
+        // return res.status(200).json(users[0]);
+      }
+    }
   } catch (err) {
     console.log(err);
   }
--- pages/api/register.js
+++ pages/api/register.js
@@ -1,23 +1,53 @@
 import clientPromise from "../../lib/mongo/index";
 
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const salt = 10;
+
 export default async (req, res) => {
   try {
     if (!req.body.email || !req.body.password)
       return res.status(300).json({ error: "Please enter all fields" });
     const email = req.body.email;
-    const password = req.body.password;
+    let password = req.body.password;
     const client = await clientPromise;
     const db = client.db("users");
     const user = await db.collection("users").find({ email: email }).toArray();
     if (user.length != 0)
       return res.status(404).json({ error: "User already exist" });
-    const data = await db
-      .collection("users")
-      .insertOne({ email: email, password: password });
-    if (data.acknowledged == false)
-      return res.status(404).json({ error: "User not found" });
 
-    return res.status(200).json({ insert: data.acknowledged });
+    bcrypt.genSalt(salt, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(password, salt, async (err, hash) => {
+        if (err) return next(err);
+        password = hash;
+        const data = await db
+          .collection("users")
+          .insertOne({ email: email, password: password });
+        if (data.acknowledged == false)
+          return res.status(404).json({ error: "User not found" });
+        console.log(data);
+
+        jwt.sign(
+          { id: data.insertedId },
+          process.env.jwtSecret,
+          (err, token) => {
+            if (err) throw err;
+            return res.json({
+              insert: data.acknowledged,
+              token,
+              user: {
+                id: data.insertedId,
+                email: email,
+              },
+            });
+          }
+        );
+      });
+    });
+
+    //return res.status(200).json({ insert: data.acknowledged });
   } catch (err) {
     console.log(err);
   }
