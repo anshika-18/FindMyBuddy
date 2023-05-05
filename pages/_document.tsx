b402b7757a18f92cf98505c6cdf28396d80d9869
Anshika Jain <anshikajain@Anshikas-MacBook-Air.local>
2023-05-05 11:43:37 +0530

    dashboard grid

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,10 +11,23 @@ export default function Document() {
           crossOrigin="anonymous"
           referrerPolicy="no-referrer"
         />
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
+          crossOrigin="anonymous"></link>
       </Head>
       <body>
         <Main />
         <NextScript />
+        <script
+          src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"
+          integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE"
+          crossOrigin="anonymous"></script>
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js"
+          integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ"
+          crossOrigin="anonymous"></script>
       </body>
     </Html>
   );
