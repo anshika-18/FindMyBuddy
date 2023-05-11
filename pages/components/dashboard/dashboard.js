acbe9082e7f1f74423dbeffc07a76c7e20d0906d
Anshika Jain <anshikajain@Anshikas-MacBook-Air.local>
2023-05-11 23:40:56 +0530

    favourites done

--- pages/components/dashboard/dashboard.js
+++ pages/components/dashboard/dashboard.js
@@ -2,7 +2,7 @@ import Style from "./dashboard.module.scss";
 import axios from "axios";
 import "react-widgets/styles.css";
 import DropdownList from "react-widgets/DropdownList";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import songs from "../../api/csv/data.json";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -10,7 +10,25 @@ import "react-toastify/dist/ReactToastify.css";
 export default function Dashboard() {
   // const { songs, setSongs } = useState([]);
   const [value, setValue] = useState("select");
+  const [favSongs, setFavSongs] = useState([]);
   // var = "select";
+
+  useEffect(async () => {
+    try {
+      const data = {
+        userId: window.localStorage.getItem("userId"),
+      };
+      const res1 = await axios.post("/api/getUserSongs", data);
+      //console.log(res1.data);
+      const res2 = await axios.post("/api/getSongsById", {
+        songIds: res1.data,
+      });
+      //console.log(res2.data);
+      setFavSongs(res2.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
   const logout = () => {
     window.localStorage.removeItem("userId");
     window.localStorage.removeItem("name");
@@ -18,6 +36,22 @@ export default function Dashboard() {
     window.location.replace("/auth/login");
   };
 
+  const removeSong = async (index) => {
+    try {
+      const data = {
+        userId: window.localStorage.getItem("userId"),
+        favSongId: index,
+      };
+      const res = await axios.post("/api/deleteSong", data);
+      window.location.reload();
+      toast.error("Song added successfully..!!", {
+        theme: "colored",
+        closeButton: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
   const addSong = async () => {
     // console.log(value);
     try {
@@ -35,6 +69,10 @@ export default function Dashboard() {
         };
         const data = axios.post("/api/addSong", input);
         window.location.reload();
+        toast.success("Song added successfully..!!", {
+          theme: "colored",
+          closeButton: false,
+        });
       }
     } catch (err) {
       console.log(err);
@@ -71,7 +109,7 @@ export default function Dashboard() {
             </button>
           </div>
           <div className={Style.das2 + " " + "col-5 "}>
-            <div>MY FAVOURITES</div>
+            <div className={Style.dasHead}>MY FAVOURITES</div>
             <div className={Style.addSong + " " + "row"}>
               <DropdownList
                 dataKey="index"
@@ -94,16 +132,29 @@ export default function Dashboard() {
             <ToastContainer></ToastContainer>
             <div className={Style.containerFav + " " + "container"}>
               <div className={Style.favRowHead + " " + "row"}>
-                <div className="col-2">#</div>
-                <div className="col-6">Title</div>
+                <div className="col-7">Title</div>
                 <div className="col-4">Artist</div>
+                <div className="col-1"></div>
               </div>
               <div className={Style.myFavDiv}>
-                <div className={Style.favRowMain + " " + "row"}>
-                  <div className="col-2">1.</div>
-                  <div className="col-6">Title</div>
-                  <div className="col-4">Artist</div>
-                </div>
+                {favSongs.map((song) => {
+                  return (
+                    <div className={Style.favRowMain + " " + "row"}>
+                      <div className="col-7">{song.title}</div>
+                      <div className="col-4">{song.artist}</div>
+                      <div className="col-1">
+                        <button
+                          className={Style.trash + " " + "btn"}
+                          onClick={() => {
+                            removeSong(song.index);
+                          }}>
+                          {" "}
+                          <i class="fa-sharp fa-solid fa-trash"></i>
+                        </button>
+                      </div>
+                    </div>
+                  );
+                })}
               </div>
             </div>
           </div>
