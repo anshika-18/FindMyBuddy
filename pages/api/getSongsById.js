393740decfe5357e724c5fc90d6900a285626352
Varsha <varsha702sahu@gmail.com>
2023-05-11 23:08:12 +0530

    getSongsById and getUsersFavSongs

---
+++ pages/api/getSongsById.js
@@ -0,0 +1,17 @@
+import songs from './csv/data.json';
+
+export default (req, res) => {
+    const { songIds } = req.body;
+    console.log(songs);
+    if (!songIds) {
+        return res.status(400).json({ error: 'Song IDs are required' });
+    }
+
+    const songDetails = songs.filter(s => songIds.includes(s.index));
+
+    if (!songDetails || songDetails.length === 0) {
+        return res.status(404).json({ error: 'Songs not found' });
+    }
+
+    return res.json(songDetails);
+};
