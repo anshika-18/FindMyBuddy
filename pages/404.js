0600cf96cd825d6f53f3cbf206874eb4ca0e271a
Anshika Jain <anshikajain@Anshikas-Air.mshome.net>
2023-05-15 16:58:20 +0530

    Dropdown css error resolved

--- pages/404.js
+++ pages/404.js
@@ -4,7 +4,7 @@ import { Button } from "react-bootstrap";
 export default function notFound() {
   return (
     <div className="notfound">
-      <h1 className="h1">404</h1>
+      {/* <h1 className="h11">404</h1>
       <div class="cloak__wrapper">
         <div class="cloak__container">
           <div class="cloak"></div>
@@ -12,11 +12,20 @@ export default function notFound() {
       </div>
       <div class="info">
         <h2 className="h2">We can't find that page</h2>
-        <p className="p">
+        <p className="p1">
           We're fairly sure that page used to be here, but seems to have gone
           missing. We do apologise on it's behalf.
         </p>
         <button className="addBtn">Home</button>
+      </div> */}
+      <div class="section">
+        <h1 class="error">404</h1>
+        <div class="page">
+          Ooops!!! The page you are looking for is not found
+        </div>
+        <a class="back-home" href="#!">
+          Back to home
+        </a>
       </div>
     </div>
   );
